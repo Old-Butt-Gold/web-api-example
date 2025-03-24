351cadac7d90e912aad0a0d6ca2d4573077e2ef3
Old-Butt-Gold <andrey2004andrey2021@gmail.com>
2025-03-24 22:28:46 +0300

    feat: start configuration of the project

--- web-api-example/WebApiExample/Program.cs
+++ web-api-example/WebApiExample/Program.cs
@@ -1,10 +1,32 @@
+using Microsoft.AspNetCore.HttpOverrides;
+using WebApiExample.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.ConfigureCors();
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseHsts();
+}
+
 app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions()
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
