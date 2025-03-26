2875a4270d8cd78cc2ea7a0f8e90b40aa6c34583
Old-Butt-Gold <andrey2004andrey2021@gmail.com>
2025-03-26 19:07:48 +0300

    feat: add Sorting

--- web-api-example/Repository/Extensions/RepositoryEmployeeExtensions.cs
+++ web-api-example/Repository/Extensions/RepositoryEmployeeExtensions.cs
@@ -1,4 +1,8 @@
-﻿using Entities.Models;
+﻿using System.Linq.Dynamic.Core;
+using System.Reflection;
+using System.Text;
+using Entities.Models;
+using Repository.Extensions.Utility;
 
 namespace Repository.Extensions;
 
@@ -19,5 +23,18 @@ public static class RepositoryEmployeeExtensions
         var lowerCaseTerm = searchTerm.Trim().ToLower();
         
         return employees.Where(e => e.Name!.ToLower().Contains(lowerCaseTerm));
+    }
+    
+    public static IQueryable<Employee> Sort(this IQueryable<Employee> employees, string
+        orderByQueryString)
+    {
+        if (string.IsNullOrWhiteSpace(orderByQueryString))
+            return employees.OrderBy(e => e.Name);
+        
+        var orderQuery = OrderQueryBuilder.CreateOrderQuery<Employee>(orderByQueryString);
+        
+        return string.IsNullOrWhiteSpace(orderQuery) 
+            ? employees.OrderBy(e => e.Name) 
+            : employees.OrderBy(orderQuery);
     }
 }
